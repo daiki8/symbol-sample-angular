06c47d9e73bd4c69905c81281aeaa0d8e4bc45fe
daiki8 <daiki1945815@gmail.com>
2021-11-07 13:41:32 +0900

    本番環境の環境変数を修正

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: true,
   // nodeUrl: 'https://symbol-sakura-16.next-web-technology.com:3001', // mainnet
   nodeUrl: 'https://sym-test.opening-line.jp:3001',                    // testnet
+
+  // for transfer demo
+  samplePrivateKey: '8E273B0E9C7DA98293D38B130B6B27505B6E2785261CBEAD649F34EF7EA04F90',
+  sampleTargetAddress: 'TCLQ3QKUFV6I35FVDXVMB7X4CWI3FLAOVQGNKCQ', // faucet address
 };
